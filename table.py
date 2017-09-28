b74cce30340e0cc3af208453bb9445e25aa1af6e
vidramshree <31615056+vidramshree@users.noreply.github.com>
2017-09-28 09:10:30 +0530

    Update table.py

--- table.py
+++ table.py
@@ -6,10 +6,10 @@ while count<=100
 	roll=input("press r to roll the dice")
 	if roll =="r":
 		r=random.randint(1,6)
+		#gives the random number from 1 to 6
 		count=count+r
 		print("your random num is",r)
-		print("you are on count")
-#import random
-#random.randint
+		print("you are on count)
+
 
 	
